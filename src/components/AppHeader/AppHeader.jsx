ebda83dd1d3674934feecd95aadc019c9469999a
Georgy Medvedsky <topchugora@gmail.com>
2023-10-04 17:04:15 +0300

    feat: create BurgerIngredients component

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -3,8 +3,6 @@ import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
 import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
-import { Typography } from '@ya.praktikum/react-developer-burger-ui-components'
-import { Box } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './AppHeader.module.css'
 
 export default function AppHeader() {
@@ -13,24 +11,24 @@ export default function AppHeader() {
         <nav className={styles.nav}>
             <ul className={styles.menu}>
                 <li className={styles.menu__item}>
-                    <a href="#" className={`${styles.menu__itemLink} pl-5 pr-5 pb-5 pt-5`}>
+                    <a href="./" className={`${styles.menu__itemLink} pl-5 pr-5 pb-5 pt-5`}>
                         <BurgerIcon type="primary" />
                         <p className="text text_type_main-default ml-2">Конструктор</p>
                     </a>
                 </li>
                 <li className={styles.menu__item}>
-                    <a href="#" className={`${styles.menu__itemLink} pl-5 pr-5 pb-5 pt-5`}>
+                    <a href="./" className={`${styles.menu__itemLink} pl-5 pr-5 pb-5 pt-5`}>
                         <ListIcon type="secondary" />
                         <p className="text text_type_main-default text_color_inactive ml-2">Лента заказов</p>
                     </a>
                 </li>
                 <li className={styles.logo}>
-                    <a href="#">
+                    <a href="./">
                         <Logo className={styles.logo}/> 
                     </a>
                 </li>
                 <li className={styles.menu__item}>
-                    <a href="#" className={`${styles.menu__itemLink} pl-5 pr-5 pb-5 pt-5`}>
+                    <a href="./" className={`${styles.menu__itemLink} pl-5 pr-5 pb-5 pt-5`}>
                         <ProfileIcon type="secondary" />
                         <p className="text text_type_main-default text_color_inactive ml-2">Личный кабинет</p>
                     </a>
--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,14 +1,67 @@
 import React from 'react'
 import styles from './BurgerIngredients.module.css'
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
-import { Counter } from '@ya.praktikum/react-developer-burger-ui-components'
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+
+function IngredientItem(props) {
+    const {image, price, name} = props.data
+    return (
+        <div className={styles.ingredientItem}>
+            <img src={image} alt={name}/>
+            <p className={`${styles.ingredientItem__price} text text_type_digits-default mt-2 mb-2`}>
+                <span className='mr-4'>{price}</span>
+                <CurrencyIcon type="primary" />
+            </p>
+            <h3 className={`${styles.ingredientItem__name} text text_type_main-default`}>
+                {name}
+            </h3>
+            
+        </div>
+    )
+}
+
+function IngredientsCategory(props) {
+    let categoryType = props.items[0].type
+    let heading
+
+    switch (categoryType) {
+        case 'bun':
+            heading = 'Булки'
+            break;
+        case 'main':
+            heading = 'Начинки'
+            break;
+        case 'sauce':
+            heading = 'Соусы'
+            break;
+        default:
+            break;
+    }
+
+    return (
+        <div className='mt-10'>
+            <h2 className='text text_type_main-medium mb-6'>{heading}</h2>
+            <ul className={styles.ingredientsCategory__list}>
+            {props.items.map(item => {
+                return (
+                    <li>
+                        <IngredientItem data={item} key={item._id}/>
+                    </li>
+                )
+            })}  
+            </ul>
+        </div>
+    )
+}
 
 export default function BurgerIngredients(props) {
     const [current, setCurrent] = React.useState('Бургер')
-
+    const buns = props.data.filter(item => item.type === 'bun')
+    const fillings = props.data.filter(item => item.type === 'main')
+    const sauces = props.data.filter(item => item.type === 'sauce')
     return (
         <section className={styles.burgerIngredients}>
-            <h2 className={`${styles.burgerIngredients__title} text text_type_main-large`}>Соберите бургер</h2>
+            <h1 className={`${styles.burgerIngredients__title} text text_type_main-large`}>Соберите бургер</h1>
             <div className={styles.burgerIngredients__tabs}>
                 <Tab value="Бургер" active={current === 'Бургер'} onClick={setCurrent}>
                     Бургер
@@ -20,25 +73,11 @@ export default function BurgerIngredients(props) {
                     Начинки
                 </Tab>
             </div>
-            {/* <ul className="ingredients">
-                <ul>
-                    <li>Булки</li>
-                    <li>
-                        <IngredientsItem />
-                    </li>
-                </ul>
-            </ul> */}
+            <div className={`${styles.burgerIngredients__items} pl-4 pr-4 custom-scroll`}>
+                <IngredientsCategory items={buns}/>
+                <IngredientsCategory items={sauces}/>
+                <IngredientsCategory items={fillings}/>
+            </div>
         </section>
   )
 }
-
-// function IngredientsItem(props) {
-
-//     return (
-//         <div className='item'>
-//             <img src={props.image} alt={props.name} />
-
-//         </div>
-//     )
-// }
-
