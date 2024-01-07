a5f37e90cf737f8ad682764465eb6f6c94749ed4
Georgy Medvedsky <topchugora@gmail.com>
2024-01-07 07:55:34 +0300

    fix: dnd ingredients

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -11,11 +11,16 @@ import OrderDetails from "../order-details/order-details"
 import { useDispatch, useSelector } from "react-redux";
 import { useDrop } from "react-dnd";
 import { addIngredient, removeIngredient, toggleBun } from "../../services/actions/ingredientsInConstructor";
+import { useEffect } from "react";
 
 export default function BurgerConstructor() {
     const { bun, ingredients } = useSelector(state => state.ingredientsInConstructor);
-    
+    const defaultBun = useSelector(state => state.ingredients.items).find(item => item.type === 'bun');
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(toggleBun(defaultBun))
+    }, [dispatch, defaultBun]);
 
     const {isModalOpen, handleOpenModal, handleCloseModal} = useModal();
 
@@ -50,21 +55,26 @@ export default function BurgerConstructor() {
                             />
                         }
                     </div>
-                    <ul className={`${styles.constructor__items} custom-scroll`} ref={dropTarget}>
-                        {ingredients.map(item => {
-                            return (
-                                <li className={styles.constructor__item} key={item.id}>
-                                    <DragIcon type="primary" />
-                                    <ConstructorElement
-                                        text={item.name}
-                                        price={item.price}
-                                        thumbnail={item.image}
-                                        handleClose={() => handleRemoveIngredient(item.id)}
-                                    />
-                                </li>
-                            )
-                        })}                    
-                    </ul>
+                    {!ingredients.length > 0
+                        ? <div className={`${styles.constructor__items_empty} custom-scroll`} ref={dropTarget}>
+                            <p className='text text_type_main-default text_color_inactive'>Перетащите ингредиенты, чтобы собрать свой бургер</p>
+                        </div>
+                        : <ul className={`${styles.constructor__items} custom-scroll`} ref={dropTarget}>
+                            {ingredients.map(item => {
+                                return (
+                                    <li className={styles.constructor__item} key={item.id}>
+                                        <DragIcon type="primary" />
+                                        <ConstructorElement
+                                            text={item.name}
+                                            price={item.price}
+                                            thumbnail={item.image}
+                                            handleClose={() => handleRemoveIngredient(item.id)}
+                                        />
+                                    </li>
+                                )
+                            })}                    
+                        </ul> 
+                    }
                     <div className={styles.constructor__item}>
                         {bun &&
                             <ConstructorElement
